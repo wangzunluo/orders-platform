4e000d777febb28d34a52d57db78192bcc40ba17
Arwin Wang <wangarwin13@gmail.com>
2026-05-03 17:39:08 -0500

    create ecs task definition. db read from ecs env config

--- user-service/app/db.py
+++ user-service/app/db.py
@@ -2,7 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if "DB_USER" in os.environ:
+    print('creating db url from ECS config')
+    DATABASE_URL = (
+        f"postgresql://{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+    )
+else:
+    print('creating db url from EC2 env file')
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
