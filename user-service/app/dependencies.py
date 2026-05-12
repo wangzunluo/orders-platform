7d86d575eac5f67903e90a3539596362a1022880
Arwin Wang <wangarwin13@gmail.com>
2026-05-11 23:54:43 -0500

    update dependencies db get engine

--- user-service/app/dependencies.py
+++ user-service/app/dependencies.py
@@ -1,6 +1,10 @@
-from .db import SessionLocal
+from sqlalchemy.orm import sessionmaker
+from .db import get_engine
 
 def get_db():
+    engine = get_engine()
+    SessionLocal = sessionmaker(bind=engine)
+
     db = SessionLocal()
     try:
         yield db
